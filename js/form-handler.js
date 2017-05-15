37c568120fc7f6258d0f27eb104a5fc2edcfff8e
Henry Otto <hotto@vanuatu.gov.vu>
2017-05-15 23:27:33 +1100

    made corrections to my assignment based on code provided in class

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,8 +1,11 @@
-// console.log("Testing out form handling");
-
  function handle() {
-   event.preventDefault();
-  var handling = document.customeremail.email.value;
 
-   console.log("Thank you for signing up to our mailing list, " + handling);
+   if (document.customeremail.email.value) {
+     event.preventDefault();
+   }
+
+   console.log("Added this email [" + document.customeremail.email.value + "] to our mailing list.");
+   var handling = document.customeremail.email.value;
+   alert("Thank you for signing up to our mailing list, " + handling + " !");
+
  }
