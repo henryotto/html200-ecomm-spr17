83eabd8a8f100bc137d9c16db5bd06b3000d4393
Henry Otto <hotto@vanuatu.gov.vu>
2017-05-15 23:26:12 +1100

    added the js file for handling items

---
+++ js/cart.js
@@ -0,0 +1,16 @@
+var cart = [
+  { name: "reversible plaid scarf", price: 26.99, id: "item01" }
+  { name: "northern lights scarf", price 29.99, id: "item02" }
+  { name: "fringed plaid scarf", price 18.99, id: "item03" }
+  { name: "multi color scarf", price 22.99, id: "item04" }
+  { name: "ombre infinity scarf", price 11.99, id: "item05" }
+  { name: "ashby twill scarf", price 70.99, id: "item06" }
+  { name: "wool cable knit scarf", price 49.99, id: "item07" }
+]
+
+function {
+
+  var addToCart = 0;
+  var removeFromCart = 0;
+
+}
