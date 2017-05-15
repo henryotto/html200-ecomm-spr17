a8ad0dc575b96128c8bc08c8a8036eb13599bfdb
Henry Otto <hotto@vanuatu.gov.vu>
2017-05-16 00:17:03 +1100

    added function

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,14 @@
 var cart = [
-  { name: "reversible plaid scarf", price: 26.99, id: "item01" }
-  { name: "northern lights scarf", price 29.99, id: "item02" }
-  { name: "fringed plaid scarf", price 18.99, id: "item03" }
-  { name: "multi color scarf", price 22.99, id: "item04" }
-  { name: "ombre infinity scarf", price 11.99, id: "item05" }
-  { name: "ashby twill scarf", price 70.99, id: "item06" }
-  { name: "wool cable knit scarf", price 49.99, id: "item07" }
+  { item: "reversible plaid scarf", price: 26.99, id: "item01" }
+  { item: "northern lights scarf", price 29.99, id: "item02" }
+  { item: "fringed plaid scarf", price 18.99, id: "item03" }
+  { item: "multi color scarf", price 22.99, id: "item04" }
+  { item: "ombre infinity scarf", price 11.99, id: "item05" }
+  { item: "ashby twill scarf", price 70.99, id: "item06" }
+  { item: "wool cable knit scarf", price 49.99, id: "item07" }
 ]
 
-function {
+function() {
 
   var addToCart = 0;
   var removeFromCart = 0;
