09e817b5f18f41b1bb32a611a6cc5ab4b300ebe1
Henry Otto <hotto@vanuatu.gov.vu>
2017-05-22 22:44:58 +1100

    made changes to match the html elements and classes

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,14 @@
-var cart = [
-  { name: "reversible plaid scarf", price: 26.99, class: "item01" },
-  { name: "northern lights scarf", price: 27.99, class: "item02" },
-  { name: "fringed plaid scarf", price: 18.99, class: "item03" },
-  { name: "multi color scarf", price: 22.99, class: "item04" },
-  { name: "ombre infinity scarf", price: 11.99, class: "item05" },
-  { name: "ashby twill scarf", price: 70.99, class: "item06" },
-  { name: "wool cable knit scarf", price: 49.99, class: "item07" }
-];
+var cart = [];
 
-$('addToCart').click(function(event){
+$('.addToCart').click(function(event){
 
   var name = $(event.target.parentElement).find('.name').text();
+  var description = $(event.target.parentElement).find('.description').text();
   var price = $(event.target.parentElement).find('.price').text();
 
   cart.push({
     name: name,
+    description: description,
     price: price});
 
   console.log(cart);
@@ -22,8 +16,8 @@ $('addToCart').click(function(event){
 });
 
 function refreshCounter() {
-  var badge = $('header').find('.counter');
+  var counter = $('header').find('.counter');
   if (cart) {
-    badge.text(cart.length);
+    counter.text(cart.length);
   }
 }
